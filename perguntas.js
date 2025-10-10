d6ac34f34e80666b7fc58b99368295c31c77f3ff
99herique99 <garcia.borges@escola.pr.gov.br>
2025-10-10 08:03:03 -0300

    ........

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'programaçao',
+    'o que e  python?',
+    'o python e uma liguagem de programaçao'
+)
+criaCartao(
+    'geografia',
+    'qual a capital da frança?',
+    'A capital da frança e paris'
+)
+criacartao(
+    'programaçao',
+    'o que e uma funçao?',
+    'uma funçao e um bloco de codigos que executa alguma tarefa'
+)
